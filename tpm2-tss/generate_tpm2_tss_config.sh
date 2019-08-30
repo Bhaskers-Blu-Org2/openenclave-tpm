ab1d50a5412ca5a2d43bbeb32d0a63fa071b682d
Paul Allen <paul.c.allen@microsoft.com>
2019-08-30 13:39:42 -0700

    Cleaned up tpm2-tss build
    Because we build tpm2-tss ourselves inside a cmakefile we don't need any
    of the autoconfigure scripts which keeps the source directory clean
    during the build.

--- tpm2-tss/generate_tpm2_tss_config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export CC=clang-7
-export CXX=clang++-7
-
-SGXTSS_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $SGXTSS_ROOT/tpm2-tss
-
-./bootstrap
-./configure --disable-tcti-mssim
